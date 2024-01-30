cfd6dd55dad795648adc32bd1e1384f1c77c06ab
Chakhsu.Lau <zexu.liu@live.cn>
2024-01-30 13:30:23 +0800

    feat: enhance deferred

--- src/deferred.ts
+++ src/deferred.ts
@@ -2,11 +2,17 @@ export class Deferred {
   public promise: Promise<any>
   public resolve!: <T>(value: T | PromiseLike<T>) => void
   public reject!: (reason?: any) => void
+  public then!: Promise<any>['then']
+  public catch!: Promise<any>['catch']
 
   constructor() {
-    this.promise = new Promise((resolve, reject) => {
-      this.resolve = resolve
-      this.reject = reject
-    })
+    this.promise = new Promise(
+      ((resolve: any, reject: any) => {
+        this.resolve = resolve
+        this.reject = reject
+      }).bind(this)
+    )
+    this.then = this.promise.then.bind(this.promise)
+    this.catch = this.promise.catch.bind(this.promise)
   }
 }
